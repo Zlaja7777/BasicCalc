e6e6f7fe22ba3f479e8f49a7c1a3069f32489d89
Zlaja7777 <31040035+Zlaja7777@users.noreply.github.com>
2019-12-11 18:31:11 +0100

    Add files via upload

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,104 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Zlatan Haljeta on 09/12/2019.
+//  Copyright © 2019 Zlatan Haljeta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var Operacija: UIButton!;
+    @IBOutlet var Prikaz: UILabel!
+    var primarna: UIColor!;
+    var OperacijaPressed: Bool = false;
+    var PrviBroj: Float? = nil;
+    var DrugiBroj: Float? = nil;
+    var odabranaOperacija: String = "";
+    
+    
+    func ProvjeraBroja() -> Bool{
+        if (Prikaz.text == "0" || OperacijaPressed == true ) {
+            
+            return true;
+        }
+        return false;
+    }
+    func ProvjeraOperacije() {
+       if OperacijaPressed == false {
+            PrviBroj = Float(Prikaz.text!);
+       }
+       else{
+            DrugiBroj = Float(Prikaz.text!);
+        }
+    }
+    @IBAction func Ocisti(_ sender: Any) {
+        Prikaz.text = "0";
+        OperacijaPressed = false;
+    }
+  
+    
+    @IBAction func Brojevi(_ sender: UIButton) {
+        if ProvjeraBroja()
+        {
+            Prikaz.text = String(sender.tag)
+        }
+        else
+        {
+            Prikaz.text = Prikaz.text! + String(sender.tag);
+        }
+        ProvjeraOperacije()
+    }
+    @IBAction func Operacije(_ sender: UIButton) {
+        OperacijaPressed = true;
+        primarna = sender.backgroundColor;
+        sender.backgroundColor = UIColor.darkGray;
+        
+        switch sender.tag {
+        case 100:
+            odabranaOperacija = "+"; break;
+        case 101:
+            odabranaOperacija = "-"; break;
+        case 102:
+            odabranaOperacija = "/"; break;
+        case 103:
+            odabranaOperacija = "x"; break;
+        default:
+            odabranaOperacija = "N/A"
+        }
+        Operacija = sender;
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    @IBAction func Result(_ sender: UIButton) {
+   
+       
+        if odabranaOperacija == "-" {
+            
+            Prikaz.text = String(PrviBroj! - DrugiBroj!)
+        }
+        else if odabranaOperacija == "+" {
+            Prikaz.text = String(PrviBroj! + DrugiBroj!)
+        }
+
+        else if odabranaOperacija == "/" {
+            Prikaz.text = String (PrviBroj! / DrugiBroj!)
+        }
+        else{
+            Prikaz.text = String (PrviBroj! * DrugiBroj!)
+        }
+        OperacijaPressed = false;
+        Operacija.backgroundColor = primarna;
+    }
+    
+}
+
+
